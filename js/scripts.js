044743a2ba2a70ce7383c072e03656a3cfcf8428
Trevor Mackin and Will Quanstrom <trevor@>
2019-10-29 15:21:02 -0700

    make a webpage that collects user checkbox data and returns and fortune based on their luck

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,35 @@
 $(document).ready(function() {
 
+  $(".form1").submit(function(event){
+    event.preventDefault();
+    var goodArray = [];
+    var badArray = [];
+    var totalGood = 0;
+    var totalBad = 0;
+
+    $(".results").show();
+    $("input:checkbox[name=good]:checked").each(function(){
+      var goodNum = $(this).val();
+      goodArray.push(goodNum);
+      totalGood = goodArray.length;
+      console.log(totalGood);
+    });
+    $("input:checkbox[name=bad]:checked").each(function(){
+      var badNum = $(this).val();
+      badArray.push(badNum);
+      totalBad = badArray.length;
+    });
 
 
+    if (totalGood > totalBad){
+      $("#goodFortune").show();
+    }
+    else if (totalGood < totalBad){
+      $("#badFortune").show();
+    }
+    else {
+      $("#evenFortune").show();
+    };
+    $(".form1").hide();
+  });
 });
